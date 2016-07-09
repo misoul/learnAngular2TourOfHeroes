eea519d145e456041d63d3eb0fd9e81979063d3f
Vinh To <misoul@gmail.com>
2016-07-08 17:26:18 -0700

    Complete Tour of Heros Practice with some tweakings.

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -17,19 +17,47 @@ export class HeroesComponent implements OnInit {
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
+  error: any;
 
   constructor(
     private router: Router,
     private heroService: HeroService) { }
 
-  ngOnInit() {
-    this.getHeroes();
-  }
-  getHeroes() {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-  // onSelect(hero: Hero) { this.selectedHero = hero; }
-  gotoDetail() {
-    this.router.navigate(['/detail', this.selectedHero.id]);
-  }
+    addHero() {
+      this.addingHero = true;
+      this.selectedHero = null;
+    }
+
+    close(savedHero: Hero) {
+      this.addingHero = false;
+      if (savedHero) { this.getHeroes(); }
+    }
+
+    deleteHero(hero: Hero, event: any) {
+      event.stopPropagation();
+      this.heroService
+          .delete(hero)
+          .then(res => {
+            this.heroes = this.heroes.filter(h => h !== hero);
+            if (this.selectedHero === hero) { this.selectedHero = null; }
+          })
+          .catch(error => this.error = error);
+    }
+
+    ngOnInit() {
+      this.getHeroes();
+    }
+
+    getHeroes() {
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    onSelect(hero: Hero) {
+      this.selectedHero = hero;
+      this.addingHero = false;
+    }
+
+    gotoDetail() {
+      this.router.navigate(['/detail', this.selectedHero.id]);
+    }
 }
